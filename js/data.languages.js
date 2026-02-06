625c86d434c3c36f86ac94fc98578e0823157386
ocitmas36-svg <ocitmas36@gmail.com>
2026-02-06 18:18:35 +0700

    Create data.languages.js

---
+++ js/data.languages.js
@@ -0,0 +1,17 @@
+const languages = [
+  { id:"javascript", name:"JavaScript", level:"Beginner → Advanced" },
+  { id:"python", name:"Python", level:"Beginner → Advanced" },
+  { id:"java", name:"Java", level:"Intermediate" },
+  { id:"cpp", name:"C++", level:"Advanced" },
+  { id:"php", name:"PHP", level:"Beginner" },
+  { id:"csharp", name:"C#", level:"Intermediate" },
+  { id:"go", name:"Go", level:"Intermediate" },
+  { id:"rust", name:"Rust", level:"Advanced" },
+  { id:"swift", name:"Swift", level:"Intermediate" },
+  { id:"kotlin", name:"Kotlin", level:"Intermediate" },
+  { id:"ruby", name:"Ruby", level:"Beginner" },
+  { id:"dart", name:"Dart", level:"Beginner" },
+  { id:"typescript", name:"TypeScript", level:"Advanced" },
+  { id:"r", name:"R", level:"Intermediate" },
+  { id:"scala", name:"Scala", level:"Advanced" }
+];
