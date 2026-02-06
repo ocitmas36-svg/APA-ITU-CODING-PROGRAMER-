7fda7b576420c5189ac09c7aea11e4a1dd848fc0
ocitmas36-svg <ocitmas36@gmail.com>
2026-02-06 18:38:35 +0700

    Create data.quiz.js

---
+++ js/data.quiz.js
@@ -0,0 +1,5 @@
+const quizData = [
+  {q:"JavaScript berjalan di browser?", options:["Benar","Salah"], answer:0, xp:10},
+  {q:"Python hanya untuk web?", options:["Benar","Salah"], answer:1, xp:10},
+  {q:"C++ digunakan untuk game?", options:["Benar","Salah"], answer:0, xp:15}
+];
